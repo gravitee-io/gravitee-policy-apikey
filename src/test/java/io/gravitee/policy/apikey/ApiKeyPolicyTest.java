c15c4cae0e9fddac842c8f6b0f28ed30e1bfdd9c
David BRASSELY <brasseld@gmail.com>
2019-03-05 00:26:14 +0100

    chore: Java 11 support
    Closes gravitee-io/issues#979

--- src/test/java/io/gravitee/policy/apikey/ApiKeyPolicyTest.java
+++ src/test/java/io/gravitee/policy/apikey/ApiKeyPolicyTest.java
@@ -35,7 +35,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.env.Environment;
 
 import java.time.Duration;
@@ -336,8 +336,6 @@ public class ApiKeyPolicyTest {
 
         when(request.headers()).thenReturn(headers);
         when(executionContext.getComponent(ApiKeyRepository.class)).thenReturn(apiKeyRepository);
-        when(executionContext.getAttribute(ExecutionContext.ATTR_API)).thenReturn(API_NAME_HEADER_VALUE);
-        when(apiKeyRepository.findById(API_KEY_HEADER_VALUE)).thenReturn(Optional.of(validApiKey));
         when(apiKeyRepository.findById(notExistingApiKey)).thenReturn(Optional.empty());
 
         apiKeyPolicy.onRequest(request, response, executionContext, policyChain);
